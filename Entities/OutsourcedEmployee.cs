eb056894c3c5932cce5bfeda5fd532765708f331
Gabriel da Silva Lima <88804562+GaboLima@users.noreply.github.com>
2024-03-29 15:30:17 -0300

    Add files via upload
    Adicionando Entities

---
+++ Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿namespace PRJFuncionarioDespesa.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double additionalCHarge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour,double additionalCHarge)
+            :base(name,hours,valuePerHour)
+        {
+            this.additionalCHarge = additionalCHarge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1*additionalCHarge;
+        }
+    }
+}
